69520f38b52fcdf073919a7e03bbc978bb06233e
Deian Petrov <deian.petrov@sap.com>
2019-11-18 09:43:41 +0200

    Login/Logout flawless + fb security session exploit covered

--- src/main/java/com/baeldung/simpleapp/config/SecurityConfig.java
+++ src/main/java/com/baeldung/simpleapp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 //@EnableWebSecurity
@@ -20,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.permitAll()
 		.anyRequest()
 		.authenticated()
-		.and().logout().logoutSuccessUrl("/").permitAll();
+		.and().logout().logoutSuccessUrl("/").permitAll()
+		.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 	}
 }
