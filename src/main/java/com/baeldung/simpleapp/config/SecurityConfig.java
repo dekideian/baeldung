47c9f610ea0a1e0670ace97586024b66b9eb7b84
Deian Petrov <deian.petrov@sap.com>
2019-11-26 15:10:46 +0200

    Removed warning

--- src/main/java/com/baeldung/simpleapp/config/SecurityConfig.java
+++ src/main/java/com/baeldung/simpleapp/config/SecurityConfig.java
@@ -3,12 +3,10 @@ package com.baeldung.simpleapp.config;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
-//@EnableWebSecurity
 @EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
